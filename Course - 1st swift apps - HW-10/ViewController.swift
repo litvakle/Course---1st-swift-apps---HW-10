d41b824b5e7394f9f61af409d8cbcb0227156b2d
Litvak Lev <sn.delui@gmail.com>
2020-02-29 12:41:41 +0500

    Initial Commit

---
+++ Course - 1st swift apps - HW-10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Course - 1st swift apps - HW-10
+//
+//  Created by Lev Litvak on 29.02.2020.
+//  Copyright © 2020 Lev Litvak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
